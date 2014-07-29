8d5d2a5352c62700676ce15a892d9fcf3246fb26
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-07-29 17:24:52 +0200

    Bump to dev version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='thecallrapi',
-    version='0.1.0',
+    version='0.1.1-dev',
     description='Python module to manage TheCallr API',
     author='Optiflows R&D',
     author_email='rand@optiflows.com',
