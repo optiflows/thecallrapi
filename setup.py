6fc5d28198bc9fb45b35ab0a480e136e25cb1a07
Guillaume Desvé <guillaume.desve@surycat.com>
2015-09-09 16:13:01 +0200

    Updates setup.py, add a manifest, version and requirements file

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
+from pip.req import parse_requirements
 from setuptools import setup, find_packages
+
+
+with open('VERSION.txt') as fp:
+    version = fp.read().strip()
+
+# Requirements
+install_reqs = parse_requirements('requirements.txt', session='dummy')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='thecallrapi',
-    version='0.1.1-dev',
+    version=version,
     description='Python module to manage TheCallr API',
     author='Optiflows R&D',
-    author_email='rand@optiflows.com',
+    author_email='rand@surycat.com',
+    install_requires=reqs,
     packages=find_packages(),
 )
